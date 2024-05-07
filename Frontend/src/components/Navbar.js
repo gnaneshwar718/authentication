fffec1f397ce2510ff75bf967243c602edd2c234
gnaneshwar.p <gnaneshwar718@gmail.com>
2024-05-07 14:40:19 +0530

    second commit

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
 
   return (
     <nav className='flex justify-around p-3 border-b border-zinc-800 items-center bg-[#1a1a1a]/90 text-zinc-300'>
-        <Link to='/'><h1 className='text-3xl'>AuthDB</h1></Link>
+        <Link to='/'><h1 className='text-3xl'>AUTHENTICATION </h1></Link>
         <ul className='flex gap-6'>
             {isUserSignedIn ? (
                 <>
